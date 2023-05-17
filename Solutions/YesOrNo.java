297d103da78c52b64ebc1c141fe4a12290027680
PrinceR2002 <103974142+PrinceR2002@users.noreply.github.com>
2023-05-16 19:08:42 -0700

    Create YesOrNo.java

---
+++ Solutions/YesOrNo.java
@@ -0,0 +1,15 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+class YesOrNo
+{
+  public static String boolToWord(boolean b)
+  {
+    //TODO
+    if(b == true){
+      return "Yes";
+    }else if (b ==false){
+      return "No";
+    }
+   return null; 
+  }
+  
+}
