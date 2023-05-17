6253d001ff7473329ab1b15af7026a577b5f3812
PrinceR2002 <103974142+PrinceR2002@users.noreply.github.com>
2023-05-16 19:16:02 -0700

    Create Function 1 - hello world.java

---
+++ Solutions/Function 1 - hello world.java
@@ -0,0 +1,8 @@
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+public class HelloWorld {
+  // Write a public static function `greet` that returns "hello world!"
+  public static String greet(){
+    return "hello world!";
+  }
+}
